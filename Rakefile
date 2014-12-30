4da33c3a8d5d73602f471c51f6d126199c73a846
Mark H <mehagel65@gmail.com>
2014-12-30 06:34:50 -0800

    database works

--- Rakefile
+++ Rakefile
@@ -1,6 +1,25 @@
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+require 'rake'
+require 'rspec/core/rake_task' 
+
 
 require File.expand_path('../config/application', __FILE__)
 
 RailsPlans::Application.load_tasks
+
+namespace :scrape do
+
+  task :get_agencies do
+    require APP_ROOT.join('db', 'scraper.rb')
+    scraper = MyScraper.new
+    scraper.scrape_agencies
+  end
+
+  task :get_projects do
+    require APP_ROOT.join('db', 'scraper.rb')
+    project_scraper = MyProjectScraper.new
+    project_scraper.get_projects
+  end
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,37 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 RailsPlans::Application.initialize!
+
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Require gems we care about
+require 'rubygems'
+
+require 'uri'
+require 'pathname'
+
+require 'pg'
+require 'active_record'
+require 'logger'
+
+# require 'sinatra'
+# require "sinatra/reloader" if development?
+
+require 'erb'
+require 'nokogiri'
+require "open-uri"
+
+# 
+# APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+APP_ROOT = File.dirname File.expand_path(__FILE__ + "/..")
+
+# APP_NAME = APP_ROOT.basename.to_s
+
+# Set up the controllers and helpers
+# Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+# Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
+
+# Set up the database and models
+# require APP_ROOT.join('config', 'database')
